8cf7fe7af7fe6b6a881974918ad35a9b688c3811
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-09-12 14:03:51 +0300

    lab04er

--- models/index.js
+++ models/index.js
@@ -11,6 +11,12 @@ require('dotenv').config();
 const POSTGRES_URL = process.env.DATABASE_URL;
 
 let sequelizeOptions = {
+    dialectOptions : {
+        ssl : {
+            require : true,
+            rejectUnauthorized: false
+        }
+    }
 
  
 };
