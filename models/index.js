a5c67ed976c7264765c31fba5426cc29e5817a14
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-09-14 14:32:29 +0300

    lab-05

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@
 
 const { Sequelize, DataTypes } = require('sequelize');
 const { createPostsTable } = require('./post.model');
+const { createCommentTable } = require('./comment.model');
+const {CrudOperations} = require('../collections/user-comment-routes');
 
 require('dotenv').config();
 
@@ -24,14 +26,23 @@ let sequelizeOptions = {
 
 let sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
+const postsModel = createPostsTable(sequelize, DataTypes);
+const posts = new CrudOperations(postsModel);
+
+const commentModel = createCommentTable(sequelize,DataTypes);
+const comments = new CrudOperations(commentModel);
+
+postsModel.hasMany(commentModel,{foreignKey:'textId',sourceKey:'id'});
+commentModel.belongsTo(postsModel,{foreignKey:'textId',targetKey:'id'});
 
 
 
 module.exports = {
 
-    dataBase: sequelize, 
-
-    posts: createPostsTable(sequelize, DataTypes),
-
+    posts, 
+    comments,
+    postsModel,
+    commentModel,
+    dataBase: sequelize,
 
 }
