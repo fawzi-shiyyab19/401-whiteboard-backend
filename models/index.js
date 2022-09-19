cd83fb86af6b069cb5fe5961034e95de9d84e678
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-09-19 14:58:42 +0300

    auth

--- models/index.js
+++ models/index.js
@@ -3,45 +3,53 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const { createPostsTable } = require('./post.model');
 const { createCommentTable } = require('./comment.model');
-const {CrudOperations} = require('../collections/user-comment-routes');
+const { createUsersTable } = require('./users.model');
+const { CrudOperations } = require('../collections/user-comment-routes');
 
 require('dotenv').config();
-
-
 
 const POSTGRES_URL = process.env.DATABASE_URL;
 
 let sequelizeOptions = {
 
-    
     dialectOptions : {
         ssl : {
-            require : true,
+            require : false,
             rejectUnauthorized: false
         }
     }
 
- 
 };
 
 let sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connected to postgres DBMS');
+    }).catch((err) => {
+        console.log(err);
+    });
+
 const postsModel = createPostsTable(sequelize, DataTypes);
 const posts = new CrudOperations(postsModel);
 
-const commentModel = createCommentTable(sequelize,DataTypes);
+const commentModel = createCommentTable(sequelize, DataTypes);
 const comments = new CrudOperations(commentModel);
 
-postsModel.hasMany(commentModel,{foreignKey:'textId',sourceKey:'id'});
-commentModel.belongsTo(postsModel,{foreignKey:'textId',targetKey:'id'});
+const usersModel = createUsersTable(sequelize, DataTypes);
+
+postsModel.hasMany(commentModel, { foreignKey: 'textId', sourceKey: 'id' });
+commentModel.belongsTo(postsModel, { foreignKey: 'textId', targetKey: 'id' });
+
 
 
 
 module.exports = {
 
-    posts, 
+    posts,
     comments,
     postsModel,
+    usersModel,
     commentModel,
     dataBase: sequelize,
 
