543945bca8da65a1263d7cfd854d59d8004e1243
fawzi-shiyyab19 <fawziakef@icloud.com>
2022-09-21 22:31:54 +0300

    return12

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,34 +1,35 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const { posts, postsModel, commentModel } = require("../models");
-const { bearerAuth } = require("../middlewares/bearerAuth");
+const express = require('express');
+const { posts, postsModel, commentModel } = require('../models');
 const router = express.Router();
 
-router.post("/post", bearerAuth, addPost);
-router.get("/post", bearerAuth, getAllPosts);
-router.get("/post/:id", bearerAuth, gitOnePost);
-router.put("/post/:id", bearerAuth, updatePost);
-router.delete("/post/:id", bearerAuth, deletePost);
+
+router.post('/post', addPost);
+router.get('/post', getAllPosts);
+router.get('/post/:id', gitOnePost);
+router.put('/post/:id', updatePost);
+router.delete('/post/:id', deletePost);
+
+
+
 
 async function addPost(req, res) {
-  res.status(201).send(await posts.addOn(req.body));
-}
+    res.status(201).send(await posts.addOn(req.body));
+};
 async function getAllPosts(req, res) {
-  let allPostsWithCommnets = await postsModel.findAll({
-    include: [commentModel],
-  });
-  res.status(200).send(allPostsWithCommnets);
-}
+    let allPostsWithCommnets = await postsModel.findAll({include:[commentModel]});
+    res.status(200).send(allPostsWithCommnets);
+};
 async function gitOnePost(req, res) {
-  res.status(200).send(await posts.getFrom(req.params.id));
-}
+    res.status(200).send(await posts.getFrom(req.params.id));
+};
 async function updatePost(req, res) {
-  res.status(202).send(await posts.updateAt(req.body, req.params.id));
-}
+    res.status(202).send(await posts.updateAt(req.body, req.params.id));
+};
 async function deletePost(req, res) {
-  await posts.deleteFrom(req.params.id);
-  res.status(204).end();
-}
+    await posts.deleteFrom(req.params.id);
+    res.status(204).end();
+};
 
 module.exports = router;
